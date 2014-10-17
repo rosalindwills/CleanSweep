d1459499772db37b5e1c3bffb0cd133c129515df
Thomas <tnunn2@gmail.com>
2014-10-17 12:00:48 -0500

    Replaced with ISensor

--- src/main/java/interfaces/ISensorSimulator.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface ISensorSimulator {
-	ICell readCell(int floorLevel, int x, int y);
-	void cleanCell(int floorLevel, int x, int y);
-	void importXml(String path) throws Exception;
-}
